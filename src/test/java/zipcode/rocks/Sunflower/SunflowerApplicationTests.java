4c92ec1191fc4dee06eedb192b7fe159b0801bbd
tb0902 <tbruton417@gmail.com>
2024-04-25 07:57:56 -0400

    first commit

---
+++ src/test/java/zipcode/rocks/Sunflower/SunflowerApplicationTests.java
@@ -0,0 +1,13 @@
+package zipcode.rocks.Sunflower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunflowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
